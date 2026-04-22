1b7c5dd457f06ea8228425941899c75d635e1a35
Frangel Raúl Crespo Barrera <frangelrcbarrera@gmail.com>
2026-04-22 15:13:53 -0400

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import AbsterChat from "./components/abster-chat";
-
-export default function App() {
-  return (
-    <main className="h-screen w-screen overflow-hidden">
-      <AbsterChat />
-    </main>
-  );
-}
